c172c468052cf053ea3f3eb2f2225036f12b61c2
Bikash Shrestha <bikash.shrestha@kotuko.it>
2024-02-14 15:20:48 +0545

    feat: setup redux-toolkit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../actions/counter";
+
+export const store = configureStore(
+    {
+        reducer: {
+            counter: counterReducer,
+        }
+    }
+);
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+
