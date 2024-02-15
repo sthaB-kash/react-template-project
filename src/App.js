ad047273b3e45aa3c1a62bfa9b7d545088ab5361
Bikash Shrestha <bikash.shrestha@kotuko.it>
2024-02-15 11:59:20 +0545

    feat: setup i18n

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback="loading">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t('learnReact', { lng: 'en', ns: 'translation'})}
+          </a>
+        </header>
+      </div>
+    </Suspense>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ export const store = configureStore(
     }
 );
 
-export type RootState = ReturnType<typeof store.getState>
+// export type RootState = ReturnType<typeof store.getState>
 
-export type AppDispatch = typeof store.dispatch
+// export type AppDispatch = typeof store.dispatch
 
