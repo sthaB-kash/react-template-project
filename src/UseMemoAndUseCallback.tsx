4e2a01075eb81eecedfb43d32704945b3170a53e
Bikash Shrestha <shresthabikash2073@gmail.com>
2024-02-19 00:13:10 +0545

    useCalback hook

--- src/UseMemoAndUseCallback.tsx
+++ src/UseMemoAndUseCallback.tsx
@@ -1,8 +1,21 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 // import 
+const SortedList = ({list, sortFunc}) => {
+  console.log('sorted list rendering..')
+  const sortedList = useMemo(() => {
+    console.log('sorting..');
+    return [...list].sort(sortFunc);
+  }, [list, sortFunc]);
 
+  return(
+    <div>
+      <p>List: {list.join(", ")}</p>
+      <p>Sorted: {sortedList.join(", ")}</p>
+    </div>
+  )
+}
 const UseMemoAndUseCallback = () => {
-  const [nums, setNums] = useState([1, 2, 3, 4, 5]);
+  const [nums, setNums] = useState([1, 7, 3, 4, 5]);
   const [name, setName] = useState('');
   // let sum = nums.reduce((acc, val) => acc + val, 0);
   let sum = useMemo(() => {
@@ -11,6 +24,9 @@ const UseMemoAndUseCallback = () => {
   }, [nums]);
   console.log(sum);
 
+  const list = ['bikash', 'shrestha', 'shrestha-bikash', 'bikash shrestha'];
+  const sortFunc = useCallback((a,b) => a > b ? 1 : -1, []);
+
 
   return (
     <div>
@@ -18,6 +34,8 @@ const UseMemoAndUseCallback = () => {
       <p>Sum of {nums.join(', ')}: {sum}</p>
       <input onChange={e => setName(e.target.value)} value={name} />
       <button onClick={() => setNums([...nums, Math.floor(Math.random() * 10)])}>Add Number</button>
+      //useCallback
+      <SortedList list={list} sortFunc={sortFunc}/>
     </div>
   )
 }
